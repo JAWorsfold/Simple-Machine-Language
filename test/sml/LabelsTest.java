4f23f433efa47d345cee3d17e7afe7dbe6b41493
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2019-04-21 08:52:57 +0100

    Added tests for remaining label methods.

--- test/sml/LabelsTest.java
+++ test/sml/LabelsTest.java
@@ -38,18 +38,38 @@ class LabelsTest {
     assertEquals(5, labels.addLabel("Six"));
   }
 
+  /**
+   * Test that indexOf returns the correct index of the provided String Label.
+   */
   @Test
   void indexOf() {
     assertEquals(0, labels.indexOf("One"));
     assertEquals(1, labels.indexOf("Two"));
     assertEquals(2, labels.indexOf("Three"));
+    assertEquals(-1, labels.indexOf("Floppy"));
   }
 
+  /**
+   * Test that toString returns the correct String of labels.
+   */
   @Test
   void toStringTest() {
+    String expect = "(One, Two, Three)";
+    assertEquals(expect, labels.toString());
+    labels.addLabel("Floppy");
+    expect = "(One, Two, Three, Floppy)";
+    assertEquals(expect, labels.toString());
   }
 
+  /**
+   * Test that reset does clear the lables using exsiting label methods.
+   */
   @Test
   void reset() {
+    assertNotEquals("()", labels.toString());
+    assertNotEquals(labels.indexOf("One"), -1);
+    labels.reset();
+    assertEquals("()", labels.toString());
+    assertEquals(labels.indexOf("One"), -1);
   }
 }
