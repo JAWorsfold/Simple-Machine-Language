021da1894f3e56b80126a86e8166f9257ce4e9d6
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2019-04-21 09:31:44 +0100

    Set up registers with multiples of two. tested get method. Tested ArrayOutofBounds exception.

--- test/sml/RegistersTest.java
+++ test/sml/RegistersTest.java
@@ -8,12 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RegistersTest {
 
+  Registers registers;
+  int regLength = 32;
+
   @BeforeEach
   void setUp() {
+    registers = new Registers();
+    int[] regValues = new int[regLength];
+    int regInput = 2;
+    for (int i = 0; i < 32; i++) {
+      regValues[i] = regInput;
+      regInput += 2;
+    }
+    registers.setRegisters(regValues);
   }
 
   @AfterEach
   void tearDown() {
+    registers = null;
   }
 
   @Test
@@ -22,6 +34,10 @@ class RegistersTest {
 
   @Test
   void getRegister() {
+    assertEquals(2, registers.getRegister(0));
+    assertEquals(8, registers.getRegister(3));
+    assertEquals(64, registers.getRegister(31));
+    assertThrows(ArrayIndexOutOfBoundsException.class, () -> { registers.getRegister(32); });
   }
 
   @Test
@@ -37,7 +53,7 @@ class RegistersTest {
   }
 
   @Test
-  void hashCode() {
+  void hashCodeTest() {
   }
 
   @Test
@@ -45,6 +61,6 @@ class RegistersTest {
   }
 
   @Test
-  void toString() {
+  void toStringTest() {
   }
 }
