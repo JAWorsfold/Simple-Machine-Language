a09504c7f556ac088fa4dd65967261b0a8ba9ef8
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2019-04-21 21:44:33 +0100

    Setting up LinInstruction tests.

--- test/sml/instructions/LinInstructionTest.java
+++ test/sml/instructions/LinInstructionTest.java
@@ -8,12 +8,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LinInstructionTest {
 
+  LinInstruction linInstructionOne;
+  LinInstruction linInstructionTwo;
+  LinInstruction linInstructionThree;
+  String label = "l1";
+  String opCode = "lin";
+  int resultReg = 0;
+  int register1 = 1;
+  int register2 = 2;
+  int register3 = 3;
+  int value1 = 0;
+  int value2 = -83;
+  int value3 = 465;
+
   @BeforeEach
   void setUp() {
+    linInstructionOne = new LinInstruction(label, register1, value1);
+    linInstructionTwo = new LinInstruction(label, register2, value2);
+    linInstructionThree = new LinInstruction(label, register3, value3);
   }
 
   @AfterEach
   void tearDown() {
+    linInstructionOne = null;
+    linInstructionTwo = null;
+    linInstructionThree = null;
   }
 
   @Test
@@ -21,6 +40,6 @@ class LinInstructionTest {
   }
 
   @Test
-  void toString() {
+  void toStringTest() {
   }
 }
