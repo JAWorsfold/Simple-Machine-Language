746b43f1b6965d848bbe3541ff899e6a86670e4e
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2019-04-21 21:52:37 +0100

    Updated and tested the LinInstruction tests.

--- test/sml/instructions/LinInstructionTest.java
+++ test/sml/instructions/LinInstructionTest.java
@@ -3,6 +3,7 @@ package sml.instructions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sml.Machine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +14,6 @@ class LinInstructionTest {
   LinInstruction linInstructionThree;
   String label = "l1";
   String opCode = "lin";
-  int resultReg = 0;
   int register1 = 1;
   int register2 = 2;
   int register3 = 3;
@@ -37,9 +37,32 @@ class LinInstructionTest {
 
   @Test
   void execute() {
+    int expected;
+    int actual;
+    Machine machine = new Machine();
+    machine.execute(); // initialise machine with empty registers
+    linInstructionOne.execute(machine);
+    actual = machine.getRegisters().getRegister(register1);
+    expected = value1;
+    assertEquals(expected, actual);
+    linInstructionTwo.execute(machine);
+    actual = machine.getRegisters().getRegister(register2);
+    expected = value2;
+    assertEquals(expected, actual);
+    linInstructionThree.execute(machine);
+    actual = machine.getRegisters().getRegister(register3);
+    expected = value3;
+    assertEquals(expected, actual);
   }
 
   @Test
   void toStringTest() {
+    String startString = label + ": " + opCode + " register ";
+    String expected = startString + register1 + " value is " + value1;
+    assertEquals(expected, linInstructionOne.toString());
+    expected = startString + register2 + " value is " + value2;
+    assertEquals(expected, linInstructionTwo.toString());
+    expected = startString + register3 + " value is " + value3;
+    assertEquals(expected, linInstructionThree.toString());
   }
 }
