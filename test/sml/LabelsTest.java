d43a3e8ce0412e78d3ea7f9483c900f94c37ffe3
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2019-04-21 08:38:10 +0100

    Had some problems with juint5. Added tests for addLabel.

--- test/sml/LabelsTest.java
+++ test/sml/LabelsTest.java
@@ -3,21 +3,36 @@ package sml;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class LabelsTest {
 
+  Labels labels;
+  String labelOne;
+  String labelTwo;
+  String labelThree;
+
   @BeforeEach
   void setUp() {
+    labels = new Labels();
+    labelOne = "One";
+    labelTwo = "Two";
+    labelThree = "Three";
   }
 
   @AfterEach
   void tearDown() {
+    labels = null;
   }
 
+  /**
+   * Test that addLabel adds the String and returns the index of the added String.
+   */
   @Test
   void addLabel() {
+    assertEquals(0, labels.addLabel(labelOne));
+    assertEquals(1, labels.addLabel(labelTwo));
+    assertEquals(2, labels.addLabel(labelThree));
   }
 
   @Test
@@ -25,7 +40,7 @@ class LabelsTest {
   }
 
   @Test
-  void toString() {
+  void toStringTest() {
   }
 
   @Test
